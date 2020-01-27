e40bc87dd5b8096322b5487504ab95c83d08ec79
Nacimiento Martin <martinnacimiento98@gmail.com>
2020-01-27 17:58:59 +0000

    Multiples puntos de entrada

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
 module.exports = {
-   entry: './index.js',
+   entry: {
+        home: path.resolve(__dirname ,'src/js/index.js'),
+        precios: path.resolve(__dirname ,'src/js/precios.js'),
+        contacto: path.resolve(__dirname ,'src/js/contacto.js')
+   },
    mode: 'development',
    output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: 'js/[name].js'
    }   
 }
